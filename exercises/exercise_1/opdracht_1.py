5844c7ecd849df45227825672e7118094e7e4630
Yur van den Berg <yur.van.den.berg@ah.nl>
2023-01-26 18:22:43 +0100

    Add exercises

--- exercises/exercise_1/opdracht_1.py
+++ exercises/exercise_1/opdracht_1.py
@@ -1,8 +1,2 @@
 def likes(team: list) -> str:
-    n_people = len(team)
-    if n_people == 0:
-        return 'no one likes this'
-    elif n_people == 1:
-        return f'{team[0]} likes this'
-    elif n_people == 2:
-        return f'{team[0]} and {team[1]} like this'
+    return True
