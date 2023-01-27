dbe936938ed3dac5b85d750a29ebd09acfff4cb1
Suchoi <IHEC1003@EMEA.ROYALAHOLD.NET>
2023-01-27 11:50:30 +0100

    opdracht 2 push

--- exercises/exercise_1/opdracht_1.py
+++ exercises/exercise_1/opdracht_1.py
@@ -5,6 +5,8 @@ def likes(team: list) -> str:
         print(str(team[0])+ " likes this")
     elif len(team) == 2:
         print(str(team[0]) + ' en ' + str(team[1]) + ' like this')
+    elif len(team) == 3:
+        print(str(team[0]) + ' en ' + str(team[1]) + ' en ' + str(team[3]) + ' like this')
     return True
 
-likes(['dima', 'evelien'])
+likes(['dima', 'evelien','wies'])
