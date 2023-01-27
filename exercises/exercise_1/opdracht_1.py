0a9c74ab6eafdf7cf0af46fcd04efa4d84e61372
EvelienvanderLaan1 <evelien.van.der.laan@ah.nl>
2023-01-27 11:26:38 +0100

    Wrote the first part of the like function

--- exercises/exercise_1/opdracht_1.py
+++ exercises/exercise_1/opdracht_1.py
@@ -1,2 +1,10 @@
 def likes(team: list) -> str:
+    if len(team) == 0:
+        print("No one likes this")
+    elif len(team) == 1:
+        print(str(team[0])+ " likes this")
+    elif len(team) == 2:
+        print(str(team[0]) + ' en ' + str(team[1]) + ' like this')
     return True
+
+likes(['dima', 'evelien'])
