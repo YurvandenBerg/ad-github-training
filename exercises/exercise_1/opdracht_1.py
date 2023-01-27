35532ae8cc29bed272b29294a11cc120b874e480
Mathol <pnl02s3j@Emea.Royalahold.net>
2023-01-27 11:23:01 +0100

    eerste deel opdracht

--- exercises/exercise_1/opdracht_1.py
+++ exercises/exercise_1/opdracht_1.py
@@ -1,2 +1,7 @@
 def likes(team: list) -> str:
-    return True
+    if len(team) == 0:
+        return "no one likes this"
+    elif len(team) == 1:
+        return str(team[0]) + ' likes this'
+    elif len(team) == 2:
+        return str(team[0]) + ' and '+str(team[1]) + ' like this'
